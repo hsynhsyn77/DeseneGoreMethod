622d0916bb381bc3351f4054b3e971fbb5732b08
huseyin <hsyn_hsyn77@hotmail.com>
2022-09-27 13:01:55 +0300

    Initial commit

---
+++ src/Proje_35.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Proje_35 {
+    public static void desen(int sayi, int gecicisayi, int x) {
+
+        if (gecicisayi >= 1 && x == 0) {
+            gecicisayi -= 5;
+            System.out.println(gecicisayi + " ");
+
+            if (gecicisayi <= 0) {
+                desen(sayi, gecicisayi, 1);
+            }
+            desen(sayi, gecicisayi, 0);
+        } else if (gecicisayi <= sayi && x == 1) {
+            gecicisayi += 5;
+            if (gecicisayi > sayi) {
+                return;
+            }
+
+            System.out.println(gecicisayi + " ");
+            desen(sayi, gecicisayi, 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int sayi;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Sayı gir  :");
+        sayi = scanner.nextInt();
+        desen(sayi, sayi, 0);
+
+    }
+}
